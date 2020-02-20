ad5a518bc3f871fbd0651af1034398b17ff70cf1
Anupamit <anupamit011@gmail.com>
2020-02-20 22:41:38 +0530

    introduction for jsx

---
+++ jsx/forlist.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Food</h1>
+    <ul>
+     <li>Meet</li>
+     <li>Rice</li>
+     <li>Daal</li>
+     <li>Samosha</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
