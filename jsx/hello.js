0a39e0c397c0daa48b0daf1753c2f16f5dc40231
Anupamit <anupamit011@gmail.com>
2020-02-21 01:12:11 +0530

    JSX code practice

--- jsx/hello.js
+++ jsx/hello.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const name ="Anupam Rai";
+const fname ="Anupam";
+const lname ="Rai";
 
-ReactDOM.render(<h1>Hello {name}!</h1>, document.getElementById('root'));
+// ReactDOM.render(<h1>Hello {name}!</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <h1>Hello {fname+ "" +lname}!</h1>,
+  // <h1>Hello {fname}!</h1>,
+  // <h1>Hello {`${fname} ${lname}`}!</h1>, //es6
+  // <h1>Hello {fname} {lname}!</h1>,
+   document.getElementById('root')
+ );
