f27abc508f9d237c920259da3fd284d0e35363d1
Anupamit <anupamit011@gmail.com>
2020-02-22 02:05:19 +0530

    Codeing for create elements

---
+++ creteelement.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const element = React.createElement('div', null, 'mera naam sahil')
+console.log(element)
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+)
