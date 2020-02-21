cba390550f3f9fb883504db49504c069ff784b93
Anupamit <anupamit011@gmail.com>
2020-02-22 02:12:34 +0530

    Jsx file for syncronized as list file component

---
+++ jsx/reactcomponents/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return(
+    <ul>
+     <li>Meet</li>
+     <li>Rice</li>
+     <li>Daal</li>
+     <li>Samosha</li>
+    </ul>
+  );
+}
+export default List;
